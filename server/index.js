fcdd1b0fa3a66a23f4ca2b906e47c14ff1a9ebf7
Shalom-Solutions <omogoyejuwon@gmail.com>
2023-08-15 23:46:16 +0100

    made updates on the server and client side

--- server/index.js
+++ server/index.js
@@ -1,8 +1,31 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import postRoutes from './routes/posts.js'
 
 const app = express(); //initialize express app
-app.use(bodyParser.json({limit: "30mb", extended: true}))
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
+
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use('/posts', postRoutes)
+
+const CONNECTION_URL =
+  "mongodb+srv://omogoyejuwon:JheyOmg1810@cluster0.vht8wac.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5001;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server listening on ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+
+//https://www.mongodb.com/cloud/atlas
